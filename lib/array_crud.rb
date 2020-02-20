00bf909de7a7d1ab5a27b43c9719b80396b515a4
Bala Luguja <bluguja@gmail.com>
2020-02-20 00:04:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array=[2,3,4,5]
-  array.push(6)
+  array = ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_start_of_array(array, element)
